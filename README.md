14e8f22c3f96caeb96cc376cd0e4ea20e4edd34c
sv-v5 <svyatver@gmail.com>
2021-06-01 18:30:57 -0500

    set up tfjs classifier in the browser

---
+++ README.md
@@ -0,0 +1,10 @@
+## Demo Classifier in Browser
+Load the model, enter a text example and view the output.
+
+
+### Play Here
+https://sv-v5.github.io/text-text-toxicity-tfjs/
+
+
+### Credits
+https://github.com/aralroca/react-text-toxicity for the js prediction code.
---
+++ src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import logo from './logo.svg';
+import React, { Fragment, useState } from "react";
+import useTextToxicity from "react-text-toxicity";
+
+
+function Toxicity({ predictions }) {
+  const style = { margin: 10 };
+
+  if (!predictions) return <div style={style}>Loading predictions...</div>;
+
+  return (
+    <div style={style}>
+      {predictions.map(({ label, match, probability }) => (
+        <div style={{ margin: 5 }} key={label}>
+          {`${label} - ${probability} - ${match ? "☣️" : "❤️"}`}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+function App() {
+  const [value, setValue] = useState("");
+  const predictions = useTextToxicity(value);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Demo tfjs Classifier Running in the Browser.
+        </p>
+        
+      </header>
+      <div className="Classifier">
+        <div>
+          <div>Write an example here</div>
+          <textarea
+            style={{ width: 500, height: 100 }}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+        {value && <Toxicity predictions={predictions} />}
+      </div>  
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+// function App() {
+//   const [value, setValue] = useState("");
+//   const predictions = useTextToxicity(value);
+
+//   return (
+//     <div style={{ display: "flex" }}>
+//       <div>
+//         <div>Write something</div>
+//         <textarea
+//           style={{ width: 300, height: 200 }}
+//           value={value}
+//           onChange={(e) => setValue(e.target.value)}
+//         />
+//       </div>
+//       {value && <Toxicity predictions={predictions} />}
+//     </div>
+//   );
+// }
+
+// export default App;
